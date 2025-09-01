2a9cdf959a2194fde9f1f133bf829bb307f1a936
sukriti-waani <sukritiwaani2006@gmail.com>
2025-09-01 21:44:29 +0530

    Add save_file function and integrate File menu setup

--- PROJECTS/textEditorApp/textEditor.py
+++ PROJECTS/textEditorApp/textEditor.py
@@ -17,3 +17,22 @@ def open_file():
         with open(file_path, 'r') as file:  # Open the selected file in read mode
             text.delete(1.0, tk.END)    # Clear existing content from text widget
             text.insert(tk.END, file.read())  # Insert file content into text widget
+
+def save_file():
+    # Ask user where to save the file (default .txt)
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files",".txt")])
+    if file_path:  
+        # Open file in write mode and save content of text widget
+        with open(file_path, 'w') as file:
+            file.write(text.get(1.0, tk.END))  
+            # Show confirmation after saving
+            messagebox.showinfo("Info", "File saves successfully!")
+
+root = tk.Tk()                     # Create main application window
+root.title("Simple Text Editor")   # Set window title
+root.geometry("800x600")           # Set default size of the window
+
+menu = tk.Menu(root)               # Create a menu bar
+root.config(menu = menu)           # Attach menu bar to window
+file_menu = tk.Menu(menu)          # Create a "File" menu
+menu.add_cascade(label = "File", menu = file_menu)  # Add "File" menu to menu bar
