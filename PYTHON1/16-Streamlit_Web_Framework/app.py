0a3e07a5eee38b7eb2776504349d0434c3a2a17e
sukriti-waani <sukritiwaani2006@gmail.com>
2025-12-09 00:25:40 +0530

    Added basic Streamlit app with data display and line chart visualization

---
+++ PYTHON1/16-Streamlit_Web_Framework/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Title of the application
+st.title("Hello Streamlit!")
+
+# Display a Simple Text
+st.write("This is a simple text")
+
+df = pd.DataFrame({
+  'first column' : [1, 2, 3, 4],
+  'second column' : [10, 20, 30, 40]
+})
+
+# Display the Dataframe
+st.write("Here is the dataframe")
+st.write(df) # Shows the DataFrame in table form
+
+# # Create random data for chart
+chart_data = pd.DataFrame(
+  np.random.randn(20, 3), columns=['a', 'b', 'c'] # Generates random values in a DataFrame
+)
+st.line_chart(chart_data) # Plots a line chart from the data
